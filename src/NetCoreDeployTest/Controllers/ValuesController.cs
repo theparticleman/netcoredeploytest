bd590f06c7f4ff2591ada63b83f444ceaab3f7a4
Jon Turner <jonathan-turner@pluralsight.com>
2017-03-20 13:39:33 -0600

    Dummy change to test deploy process

--- src/NetCoreDeployTest/Controllers/ValuesController.cs
+++ src/NetCoreDeployTest/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace NetCoreDeployTest.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "brown bag" };
         }
 
         // GET api/values/5
